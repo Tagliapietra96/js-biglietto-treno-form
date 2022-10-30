e91ed3e2b8eba5594331a9f96560cfbc93598972
Tagliapietra96 <tagliatek96@gmail.com>
2022-10-30 16:37:06 +0100

    added km controller

--- js/js.js
+++ js/js.js
@@ -1,7 +1,9 @@
 const userNameEl = document.getElementById('user-name');
-const nameTitleEl = document.querySelector('h6.my-name-title')
+const nameTitleEl = document.querySelector('h6.my-name-title');
 const userKmEl = document.getElementById('user-km');
+const kmTitleEl = document.querySelector('h6.my-km-title');
 const userAgeEl = document.getElementById('user-age');
+const ageTitleEl = document.querySelector('h6.my-age-title');
 const okBtnEl = document.querySelector("#form-section .btn-primary");
 const abortBtnEl = document.querySelector("#form-section .btn-secondary");
 const priceByKm = .21;
@@ -18,6 +20,12 @@ abortBtnEl.addEventListener("click", function() {
     userNameEl.value = "";
     userKmEl.value = "";
     userAgeEl.value = 0;
+    nameTitleEl.innerHTML = "Inserisci il tuo Nome";
+    nameTitleEl.classList.remove("text-danger");
+    kmTitleEl.innerHTML = "Inserisci la tratta";
+    kmTitleEl.classList.remove("text-danger");
+    ageTitleEl.innerHTML = "Seleziona la tua età";
+    ageTitleEl.classList.remove("text-danger");
 });
 
 okBtnEl.addEventListener("click", function() {
@@ -33,6 +41,13 @@ okBtnEl.addEventListener("click", function() {
         nameTitleEl.innerHTML = "Il tuo nome è troppo breve";
         nameTitleEl.classList.remove("text-danger");
         nameTitleEl.classList.add("text-danger");
-    }
+    } else if(parseInt(userKmEl.value) === 0 || userKmEl.value === ""){
+        alert("Inserisci i chilometri che devi percorrere");
+    } else if(parseInt(userKmEl.value) < 0){
+        alert("Inserisci correttamente i chilometri che devi percorrere");
+        kmTitleEl.innerHTML = "Non si accettano valori negativi";
+        kmTitleEl.classList.remove("text-danger");
+        kmTitleEl.classList.add("text-danger");
+    } 
 });
 
